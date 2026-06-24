4cf723351f2630fa6926006b47635fd1104f2558
Anthony <56187194+raszion23@users.noreply.github.com>
2026-06-24 12:18:28 -0400

    Added playRound() function. Need to tweak.

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-let computerChoiceString = "";
-let humanChoiceString = "";
-let computerChoiceInt = 0;
-let humanChoiceInt = 0;
+let computerChoice = "";
+let humanChoice = "";
 let computerScore = 0;
 let humanScore = 0;
+let roundAnnouncement = "";
 
 function getComputerChoice() {
     const min = 1;
     const max = 3;
     let randNum = Math.floor(Math.random() * 3) + 1;
-    computerChoiceInt = randNum;
 
     if (randNum == 1 ) {
-        computerChoiceString = "Rock";
+        computerChoice = "Rock";
     } else if(randNum == 2) {
-        computerChoiceString = "Paper";
+        computerChoice = "Paper";
     } else if (randNum == 3) {
-        computerChoiceString = "Scissors";
+        computerChoice = "Scissors";
     }
 }
 
@@ -24,22 +22,33 @@ function getHumanChoice() {
     const input = prompt("Rock, Paper, or Scissors? ");
 
     if (input == "Rock") {
-        humanChoiceString = input;
-        humanChoiceInt = 1;
+        humanChoice = input;
     } else if (input == "Paper") {
-        humanChoiceString = input;
-        humanChoiceInt = 2;
+        humanChoice = input;
     } else if (input == "Scissors") {
-        humanChoiceString = input;
-        humanChoiceInt = 3;
+        humanChoice = input;
     } 
 }
 
-getComputerChoice();
+function playRound() {
+
+    if (humanChoice == "Rock" && computerChoice == "Rock") {
+        roundAnnouncement = "TIE!"
+    } else if (humanChoice = "Rock" && computerChoice == "Paper") {
+        roundAnnouncement = "You lose! Paper beats Rock!"
+        computerScore++;
+    } else if (humanChoice == "Rock" && computerChoice == "Scissors") {
+        roundAnnouncement = "You win! Rock beats Scissors!"
+        humanScore++;
+    }
+}
+
 getHumanChoice();
+getComputerChoice();
+playRound();
 
-console.log(humanChoiceString);
-console.log(humanChoiceInt);
+console.log(humanChoice);
+console.log(computerChoice);
+console.log(roundAnnouncement);
+console.log(humanScore + ", " + computerScore);
 
-console.log(computerChoiceString);
-console.log(computerChoiceInt);
